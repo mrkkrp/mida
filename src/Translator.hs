b44fb67d1f138897f165c9cc030164b4fc81f1c8
Mark Karpov <g15h89jkj45@gmail.com>
2014-10-19 19:33:58 +0700

    finished translator

--- src/Translator.hs
+++ src/Translator.hs
@@ -14,4 +14,61 @@
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 -- General Public License for more details.
 
-module Translator where
+module Translator
+    ( saveMidi
+    , topDefs)
+where
+
+-- Import Section --
+
+import Parser
+import Environment
+import Control.Monad.State
+import Control.Applicative ((<$>), (<*>))
+import Codec.Midi
+import Data.List
+import Data.Maybe
+
+-- Data Structures --
+
+type Batch = ([Int], [Int], [Int])
+type Event = (Ticks, Message)
+
+-- Translation --
+
+mvIndex = 7
+durName = "dur"
+velName = "vel"
+pchName = "pch"
+topDefs = [x ++ show n | x <- [durName,velName,pchName], n <- [0..mvIndex]]
+
+request :: Int -> MidaM Batch
+request n =
+    do dur <- evalItem $ durName ++ i
+       vel <- evalItem $ velName ++ i
+       pch <- evalItem $ pchName ++ i
+       return (dur, vel, pch)
+    where i = show n
+
+fullyDefined :: Batch -> Bool
+fullyDefined (dur, vel, pch) = f dur && f vel && f pch
+    where f [] = False
+          f _  = True
+
+slice :: Int -> Batch -> Batch
+slice t (dur, vel, pch) = (take n dur, take n vel, take n pch)
+    where n = length . fromJust $ find ((>= t) . sum) (inits dur)
+
+toTrack :: Batch -> Track Int
+toTrack (dur, vel, pch) = (concat $ zipWith3 f dur vel pch) ++ [(0, TrackEnd)]
+    where f d v p = [(0, NoteOn 0 p v),
+                     (d, NoteOn 0 p 0)]
+
+saveMidi :: Int -> Int -> Int -> String -> MidaM ()
+saveMidi s q beats fileName =
+    do -- setRand $ mkStdGen s
+       voices <- filter fullyDefined <$> mapM request [0..mvIndex]
+       let xs = map (toTrack . slice (beats * q)) voices
+       liftIO $ exportFile fileName Midi { fileType = MultiTrack,
+                                           timeDiv  = TicksPerBeat q,
+                                           tracks   = xs }
