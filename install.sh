54e65709722dd2f02598231a43b12637e243063d
Mark Karpov <g15h89jkj45@gmail.com>
2014-11-03 13:17:42 +0600

    improved scripts

--- install.sh
+++ install.sh
@@ -13,52 +13,55 @@ I_ITEMS="/usr/{bin/{mida,midarm},share/{licenses,doc,haskell/source}/mida/}"
 
 bad_exit() # prints error message and exits the program
 {
-    echo "FAILED" 1>&2
+    echo "FAILED." 1>&2
     exit 1
 }
 
 ### main
 
+echo '-> MIDA installation has been started;'
+
 # 1. check if actual user is root (must be root to install the software)
 
-echo -n 'Actual user must be root.......'
+echo -n '=> actual user must be root....'
 test $(id -u) -gt 0 && bad_exit
-echo 'OK'
+echo 'OK,'
 
 # 2. check if there is compiled executable
 
-echo -n 'Searching for executable.......'
+echo -n '=> searching for executable....'
 test -f $SCRIPT_DIR/dist/build/mida/mida || bad_exit
-echo 'OK'
+echo 'OK,'
 
 # 3. creating directories
 
-echo -n 'Creating directories...........'
+echo -n '=> creating directories........'
 eval mkdir -p $I_DIRS > /dev/null 2>&1
 if test $? -eq 0
-then echo 'OK'
+then echo 'OK,'
 else bad_exit
 fi
 
 # 4. copying new files
 
-echo -n 'Copying new files..............'
+echo -n '=> copying new files...........'
 cp -u $SCRIPT_DIR/dist/build/mida/mida /usr/bin/
 cp -u $SCRIPT_DIR/midarm               /usr/bin/
 cp -u $SCRIPT_DIR/LICENSE.md           /usr/share/licenses/mida/
 cp -u $SCRIPT_DIR/doc/*.{html,css}     /usr/share/doc/mida/
+cp -u $SCRIPT_DIR/Mida.cabal           /usr/share/haskell/source/mida/
 cp -u $SCRIPT_DIR/src/*.hs             /usr/share/haskell/source/mida/
-echo 'OK'
+echo 'OK,'
 
 # 5. settting permissions
 
-echo -n 'Setting permissions............'
+echo -n '=> setting permissions.........'
 eval chmod -R 755 $I_ITEMS
 if test $? -eq 0
-then echo 'OK'
+then echo 'OK,'
 else bad_exit
 fi
 
 # 6. done
 
-echo 'Done.'
+echo '-> done.'
