cb290f0e99859c18d5588a14488400a9edc97159
mrkkrp <markkarpov@opmbx.org>
2015-03-21 17:57:46 +0600

    minor edit of license

--- src/Mida/Interaction/Commands.hs
+++ src/Mida/Interaction/Commands.hs
@@ -144,18 +144,18 @@ cmdLicense _ = liftIO $ putStr
     "MIDA - realization of MIDA, language for generation of MIDI files.\n\
     \Copyright (c) 2014, 2015 Mark Karpov\n\
     \\n\
-    \This program is free software: you can redistribute it and/or modify\n\
-    \it under the terms of the GNU General Public License as published by\n\
-    \the Free Software Foundation, either version 3 of the License, or\n\
-    \(at your option) any later version.\n\
+    \MIDA is free software: you can redistribute it and/or modify it under the\n\
+    \terms of the GNU General Public License as published by the Free Software\n\
+    \Foundation, either version 3 of the License, or (at your option) any\n\
+    \later version.\n\
     \\n\
-    \This program is distributed in the hope that it will be useful,\n\
-    \but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-    \MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n\
-    \GNU General Public License for more details.\n\
+    \MIDA is distributed in the hope that it will be useful, but WITHOUT ANY\n\
+    \WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS\n\
+    \FOR A PARTICULAR PURPOSE. See the GNU General Public License for more\n\
+    \details.\n\
     \\n\
-    \You should have received a copy of the GNU General Public License\n\
-    \along with this program. If not, see <http://www.gnu.org/licenses/>.\n"
+    \You should have received a copy of the GNU General Public License along\n\
+    \with this program. If not, see <http://www.gnu.org/licenses/>.\n"
 
 cmdLoad :: String -> MidaIO ()
 cmdLoad given = do
